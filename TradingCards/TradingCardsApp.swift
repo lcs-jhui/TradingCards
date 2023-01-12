5a57db6fb67a5f0c3ddd71d53138ab8ce82de2bf
Justin Hui <jhui@lcs.on.ca>
2023-01-12 14:19:59 -0500

    Initial Commit

---
+++ TradingCards/TradingCardsApp.swift
@@ -0,0 +1,17 @@
+//
+//  TradingCardsApp.swift
+//  TradingCards
+//
+//  Created by Justin Hui on 12/1/2023.
+//
+
+import SwiftUI
+
+@main
+struct TradingCardsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
